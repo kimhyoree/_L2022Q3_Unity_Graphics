40861834450342564070feba94ef11803450b834
kimhyoree <73922519+kimhyoree@users.noreply.github.com>
2022-10-17 14:50:42 +0900

    6

---
+++ Assets/week06A_Light/Light_Controller.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light_Controller : MonoBehaviour
+{
+    public GameObject Light;
+    bool isOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOn = false;
+        Light.SetActive(isOn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            print("L down"); //L키를 누를 때마다
+            print(Light.name);
+            isOn = !isOn; //트루를 폴스로 폴스를 트루로 바꿔주는 코드
+            Light.SetActive(false);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        print(gameObject.name + "mouse down");
+    }
+
+}
